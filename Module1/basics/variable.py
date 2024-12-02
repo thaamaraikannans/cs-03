a4b309d8cb065d3f12e1a391d2409455b9d2e015
thaamaraikannan <thaamaraikannans.xbi4@gmail.com>
2024-12-02 11:17:33 +0530

    till variables

---
+++ Module1/basics/variable.py
@@ -0,0 +1,14 @@
+myname = "Chandru"
+myage = myname
+mycurrentresidingcityintamilnaduwas = "Coimbatore"
+
+
+MyCurrentResidingCityInTamilnaduWas = "Coimbatore"
+myCurrentResidingCityInTamilnaduWas = "Coimbatore"
+
+my_current_residing_city_in_tamilnadu_was = "Coimbatore"
+
+
+
+print(myname)
+print(myage)
---
+++ program.py
@@ -0,0 +1,1 @@
+print("Welcome to great karaikalam magic show! ")
